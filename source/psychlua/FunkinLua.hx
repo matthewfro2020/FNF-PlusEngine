79d60ff83669bd7345703296562b923ebfa9a006
Lenin <medinastoleninliu@gmail.com>
2025-09-27 20:22:43 -0500

    version 0.8.5f

--- source/psychlua/FunkinLua.hx
+++ source/psychlua/FunkinLua.hx
@@ -1705,6 +1705,7 @@ class FunkinLua {
 		CustomSubstate.implement(this);
 		ShaderFunctions.implement(this);
 		DeprecatedFunctions.implement(this);
+		LuaModchart.implement(this);
 		MobileFunctions.implement(this);
 		#if android AndroidFunctions.implement(this); #end
 
